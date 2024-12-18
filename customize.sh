d6c8647bb4e0647bde0c18795d71803c56116284
唵 <153998419+rhythmcache@users.noreply.github.com>
2024-12-18 13:13:12 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,12 +1,7 @@
-#!/system/bin/sh
 ui_print "*******************************"
 ui_print "     BootLoop Rescue          "
 ui_print "*******************************"
 ui_print " "
-cp $MODPATH/service.sh $MODPATH/service.sh
-cp $MODPATH/module.prop $MODPATH/module.prop
 ui_print " "
 ui_print "- Installation complete!"
 set_perm_recursive $MODPATH 0 0 0755 0644
-rm $MODPATH/customize.sh
-exit 0
