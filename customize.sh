85a2da398127ed271b533b07aeeb6617ee05d5c7
f33a6a <153998419+rhythmcache@users.noreply.github.com>
2025-01-08 06:20:18 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,14 +4,17 @@ ui_print "*******************************"
 ui_print " => Choose SystemUI Monitor"
 ui_print "Press VOLUME UP to enable :"
 ui_print "Press VOLUME DOWN to disable :"
-getevent -qlc 1 | while read line; do
-    if echo "$line" | grep -q "KEY_VOLUMEUP"; then
+ui_print "Waiting for your choice..."
+while true; do
+    event=$(getevent -qlc 1 2>/dev/null)
+    if echo "$event" | grep -q "KEY_VOLUMEUP"; then
         ui_print "SystemUI Monitor enabled."
         if [ -f /data/adb/systemui.monitor.disable ]; then
             rm -f /data/adb/systemui.monitor.disable
         fi
         break
-    elif echo "$line" | grep -q "KEY_VOLUMEDOWN"; then
+    fi
+    if echo "$event" | grep -q "KEY_VOLUMEDOWN"; then
         ui_print " => SystemUI Monitor disabled."
         touch /data/adb/systemui.monitor.disable
         break
@@ -20,4 +23,3 @@ done
 set_perm_recursive $MODPATH 0 0 0755 0644
 ui_print " "
 ui_print "- Installation complete!"
-
