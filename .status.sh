e0de881d73573c8ee660ec023edb32cb702a742d
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-01-11 18:40:13 +0530

    Update .status.sh

--- .status.sh
+++ .status.sh
@@ -6,22 +6,20 @@ REQUIRED_FILES="\
     $MODULE_DIR/service.sh \
     $MODULE_DIR/post-fs-data.sh \
     $MODULE_DIR/uninstall.sh"
-
 for file in $REQUIRED_FILES; do
     if [ ! -f "$file" ]; then
         sed -i '/^description=/d' "$MODULE_PROP"
-        echo "description= Module is corrupted. Please reinstall." >> "$MODULE_PROP"
+        echo "description= ⚠️ Warning : Module is corrupted. Please reinstall." >> "$MODULE_PROP"
         exit 0
     fi
 done
-
-if [ -f "$SYSTEMUI_MONITOR_DISABLE" ] && [ -f "$MODULE_DIR/disable" ]; then
+if [ -f "$MODULE_DIR/disable" ]; then
     sed -i '/^description=/d' "$MODULE_PROP"
-    echo "description= SystemUI Monitor ❌ | Bootloop Monitor ❌ | Module is enabled ❌" >> "$MODULE_PROP"
+    echo "description= SystemUI Monitor ❌ | Bootloop Monitor ❌ | ⚠️ Module is Disabled " >> "$MODULE_PROP"
 elif [ -f "$SYSTEMUI_MONITOR_DISABLE" ]; then
     sed -i '/^description=/d' "$MODULE_PROP"
-    echo "description= SystemUI Monitor ❌ | Bootloop Monitor ✅ | Module is enabled? ✅" >> "$MODULE_PROP"
+    echo "description= SystemUI Monitor ❌ | Bootloop Monitor ✅ | Module is Working ✨" >> "$MODULE_PROP"
 else
     sed -i '/^description=/d' "$MODULE_PROP"
-    echo "description= SystemUI Monitor ✅ | Bootloop Monitor ✅ | module is enabled? ✅" >> "$MODULE_PROP"
+    echo "description= SystemUI Monitor ✅ | Bootloop Monitor ✅ | Module is Working ✨" >> "$MODULE_PROP"
 fi
